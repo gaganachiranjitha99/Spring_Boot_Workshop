09e745f4a34a098615da9739762ddf8374f627f4
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-05-20 09:52:38 +0530

    init commit

---
+++ src/test/java/com/ict/Spring_Boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
